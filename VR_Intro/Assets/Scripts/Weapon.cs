0045cc8073be698decf999b2a0bdfee5368859a5
Marc Brgs <66321703+marc-brgs@users.noreply.github.com>
2022-12-30 14:51:49 +0100

    Weapon class

---
+++ VR_Intro/Assets/Scripts/Weapon.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] protected float shootingForce;
+    [SerializeField] protected Transform bulletSpawn;
+    [SerializeField] private float recoilForce;
+    [SerializeField] private float damage;
+    
+    private Rigidbody rigidbody;
+    private XRGrabInteractable interactableWeapon;
+    
+    protected void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        interactableWeapon = GetComponent<XRGrabInteractable>();
+    }
+
+    public void Shoot()
+    {
+        ApplyRecoil();
+    }
+
+    private void ApplyRecoil()
+    {
+        rigidbody.AddRelativeForce(Vector3.back * recoilForce, ForceMode.Impulse);
+    }
+
+    public float getDamage()
+    {
+        return damage;
+    }
+}
