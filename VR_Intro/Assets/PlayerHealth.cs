73b2c0832f6ca09df7c64fafff37ee91f411c16b
MatthieuL <61740378+Matthieu39@users.noreply.github.com>
2023-02-27 15:26:37 +0100

    Functional HealthBar

--- VR_Intro/Assets/PlayerHealth.cs
+++ VR_Intro/Assets/PlayerHealth.cs
@@ -1,35 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-
-
 
 public class PlayerHealth : MonoBehaviour
 {
+    public int maxHealth = 100;
+    public int currentHealth;
 
-    public Slider slider; 
+    public HealthBar healthBar;
+    
     
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+
     }
 
-
-    public void SetMaxHealth(int health)
-    {
-        slider.maxValue = health;
-        slider.value = health;
-    }
-    
-    public void SetHealth(int health)
-    {
-        slider.value = health;
-    }
-
-    
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage(20);
+        }
+    }
+
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
     }
 }
